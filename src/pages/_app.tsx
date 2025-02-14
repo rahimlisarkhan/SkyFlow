0fff1630bc09397c74477e5fb8de7fad0fbbe91f
rahimlisarkhan <rahimlisarkhan@gmail.com>
2025-02-14 16:09:19 +0400

    feat: add react-toastify for improved notification handling and update imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,21 @@
-import React from "react";
-import { ConfigProvider } from "antd";
-import type { AppProps } from "next/app";
-import nextI18NextConfig from "../../next-i18next.config";
+import React from 'react';
+import { ConfigProvider } from 'antd';
+import type { AppProps } from 'next/app';
+import nextI18NextConfig from '../../next-i18next.config';
 
-import "antd/dist/reset.css";
-import theme from "@/common/theme/themeConfig";
-import { store } from "@/common/store";
-import { Provider } from "react-redux";
-import { appWithTranslation } from "next-i18next";
+import 'antd/dist/reset.css';
+import theme from '@/common/theme/themeConfig';
+import { store } from '@/common/store';
+import { Provider } from 'react-redux';
+import { appWithTranslation } from 'next-i18next';
+
+import { ToastContainer } from 'react-toastify';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ConfigProvider theme={theme}>
     <Provider store={store}>
       <Component {...pageProps} />
+      <ToastContainer />
     </Provider>
   </ConfigProvider>
 );
